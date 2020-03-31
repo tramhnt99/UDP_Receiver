81861be529134e5adeac9966b6227f90ecb64b0e
Tram Hoang <tram.hoang@u.yale-nus.edu.sg>
2020-03-31 15:26:50 +0700

    restructure

--- Sender.py
+++ Sender.py
@@ -13,25 +13,20 @@ This is a skeleton sender class. Create a fantastic transport protocol here.
 class Sender(BasicSender.BasicSender):
     def __init__(self, dest, port, filename, debug=False, sackMode=False):
         super(Sender, self).__init__(dest, port, filename, debug)
-        self.debug = False
-        self.currAckSeqno = 0
-        self.lastSentSeqno = 0
-        self.max_buf_size = 5
-        self.timeout = 10 #time.time() returns flatings
-        self.seq_acks = {} #hashing acks
-        self.updated = time.time()
-        if sackMode:
-            raise NotImplementedError  # remove this line when you implement SACK
         if filename is None:
             self.infile = sys.stdin
         else:
             self.infile = open(filename, "r")
-
-    #implement a time_out at init
-
-    #handle_response must:
-        #check the Checksum
-        #Ack -> check the sequence number -> ...
+        self.debug = False
+        self.currAckSeqno = 0
+        self.toSendSeqno = 0
+        self.max_buf_size = 5
+        self.timeout = 10 #time.time() returns flatings
+        self.seq_acks = {} #hashing acks
+        self.next_msg = self.infile.read(500)
+        self.msg_type = 'start'
+        if sackMode:
+            raise NotImplementedError  # remove this line when you implement SACK
 
     def handle_response(self, response_packet):
         if not Checksum.validate_checksum(response_packet):
@@ -39,83 +34,94 @@ class Sender(BasicSender.BasicSender):
             return #drop the packet
         else:
             print("recv: %s" % response_packet)
+            msg_type, seqno = self.split_message(response_packet)
 
-            #check for time out
-            if time.time() - self.updated > self.timeout:
-                print("Sender.py packet timed out")
-                self.handle_timeout()
-            else:
-                self.updated = time.time()
-                msg_type, seqno = self.split_message(response_packet)
+            if seqno > self.currAckSeqno: #assuming we won't receive seqno beyond our window
+                if msg_type == 'ack':
+                    if seqno in self.seq_acks:
+                        self.handle_dup_ack(response_packet)
+                    else:
+                        self.seq_acks[seqno] = msg_type #store something arbitrary
+                        self.handle_new_ack(response_packet)
 
-                #check if is in window, else we drop the packet
-
-                # print(seqno)
-                # print(self.currAckSeqno + self.max_buf_size)
-                # print(seqno < self.currAckSeqno + self.max_buf_size)
-
-                #THIS IS EVALUATING INCORRECT ->   and seqno <= self.currAckSeqno + self.max_buf_size
-                #ok, assuming we won't be receiving acks beyond our window
-                if seqno > self.currAckSeqno:
-                    if msg_type == 'ack':
-                        if seqno in self.seq_acks:
-                            self.handle_dup_ack(response_packet)
-                        else:
-                            self.seq_acks[seqno] = msg_type #store something arbitrary
-                            self.handle_new_ack(response_packet)
 
     #TODO: implement window
     #TODO: to implement timeout -> we need an array for each packet we've sent (at most 5) to keep track
-    #TODO: rewrite the start -> only handles sending the 'start'
+    #TODO: rewrite the start -> only handles sending the 'start' and receiving
     #   handle functions handle the sending/resending of packets
     def start(self):
-        msg = self.infile.read(500)
-        msg_type = None
-        while not msg_type == 'end':
-            next_msg = self.infile.read(500)
 
-            msg_type = 'data'
-            if self.currAckSeqno == 0:
-                msg_type = 'start' #starting the connection
-            elif next_msg == "":
-                msg_type = 'end' #ending the connection
+        assert self.msg_type == 'start'
+        packet = self.make_packet(self.msg_type, self.toSendSeqno, self.next_msg)
+        self.send(packet)
+        self.toSendSeqno += 1
+        # print "sent: %s" % packet
 
-            packet = self.make_packet(msg_type, self.currAckSeqno, msg)
+        #send the next 4 packets
+        self.msg_type = 'data'
+        for i  in range(4):
+            self.next_msg = self.infile.read(500)
+            if self.next_msg == "":
+                self.msg_type = 'end'
+            packet = self.make_packet(self.msg_type, self.toSendSeqno, self.next_msg)
             self.send(packet)
             # print "sent: %s" % packet
+            self.toSendSeqno += 1
+            if self.msg_type == 'end':
+                break
+
+
+        while self.currAckSeqno < self.toSendSeqno:
 
             response = self.receive()
             self.handle_response(response)
 
-            msg = next_msg
-            self.currAckSeqno += 1
-
         self.infile.close()
 
 
-    def handle_timeout(self):
-        pass
 
     def handle_new_ack(self, ack):
         for n in sorted(self.seq_acks.keys()):
             if n == str(self.currAckSeqno + 1):
                 del self.seq_acks[n]
-                print("self.seq_acks is " + str(self.seq_acks))
+                self.currAckSeqno += 1
+
+                #sending more packets
+                if self.msg_type == 'end': #we've sent all packets - now we just wait for all acks
+                    return
+                else:
+                    if self.toSendSeqno <= self.currAckSeqno + self.max_buf_size:
+                        self.next_msg = self.infile.read(500)
+                        self.msg_type = 'data'
+                        if self.next_msg == "":
+                            self.msg_type = 'end'
+
+                        packet = self.make_packet(self.msg_type, self.toSendSeqno, self.next_msg)
+                        self.send(packet)
+                        # print "sent: %s" % packet
+                        msg = self.next_msg
+                        # print(self.utf8len(msg))
+                        self.toSendSeqno += 1
             else:
-                break #CONTINUE HERE: double check the corner case of when this first starts
-                        #check for when acks are recieved not in order
+                break
 
     def handle_dup_ack(self, ack):
         pass
 
+    def handle_timeout(self):
+        pass
+
     def log(self, msg):
         if self.debug:
-            print msg
+            print(msg)
 
     def split_message(self, message): #we receive ack|<seqno>|<checksum|
         pieces = message.split('|')
         msg_type, seqno = pieces[0:2]  #First 2 are ack and seqno; checksum is alr checked -> leave it
         return msg_type, seqno
+
+    def utf8len(self, s):
+        return len(s.encode('utf-8'))
 
 
 '''
@@ -164,3 +170,10 @@ if __name__ == "__main__":
         s.start()
     except (KeyboardInterrupt, SystemExit):
         exit()
+
+
+            # #check for time out
+            # if time.time() - self.updated > self.timeout:
+            #     print("Sender.py packet timed out")
+            #     self.handle_timeout()
+            # else:
